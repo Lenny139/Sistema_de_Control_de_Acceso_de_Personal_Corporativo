6002de5320288dbfb23621d25f71d40d080f8923
Lenny Alejandro Rincon Ramirez <lenny.rincon.5@gmail.com>
2026-05-01 13:38:59 -0500

    Back: ajustar servidor

--- src/api/infrastructure/http/Server.ts
+++ src/api/infrastructure/http/Server.ts
@@ -18,6 +18,7 @@ export class Server {
     this.app.use(helmet());
     this.app.use(cors());
     this.app.use(express.json());
+    this.app.use(express.static('public'));
   };
 
   private readonly routes = (): void => {
